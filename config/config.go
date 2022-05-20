80c9495996841e2b64354f9cca83ab6821177f5a
kawada <vgnsl94@gmail.com>
2022-05-20 18:12:17 +0900

    feature configファイル完成

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"log"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
 type ConfigList struct {
 	Port      string
 	SQLDriver string
@@ -8,3 +14,18 @@ type ConfigList struct {
 }
 
 var Config ConfigList
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustString("8080"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		DbName:    cfg.Section("db").Key("name").String(),
+		LogFile:   cfg.Section("web").Key("logfile").String(),
+	}
+
+}
